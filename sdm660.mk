7721f55b52b1320b2b29508276a25475da09877c
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2025-11-23 16:25:18 +0530

    Modify product inheritance in sdm660.mk
    Comment out the Dolby product inheritance and add ViPER4AndroidFX configuration.

--- sdm660.mk
+++ sdm660.mk
@@ -5,7 +5,9 @@
 #
 
 # Dolby
-$(call inherit-product, hardware/dolby/dolby.mk)
+# $(call inherit-product, hardware/dolby/dolby.mk)
+
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
